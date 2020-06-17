a0ce53ad5b429a9a56f8e20d2ea34cb85ec6bb74
virnarula <virsvirus@gmail.com>
2020-06-17 18:55:30 -0500

    Create move wrapper class

---
+++ model/requests.js
@@ -0,0 +1,8 @@
+class move {
+    constructor(board_x, board_y, rel_x, rel_x, char) {
+        this.board_x = board_x;
+        this.board_y = board_y;
+        this.rel_x = rel_x;
+        this.char = char;
+    }
+}
