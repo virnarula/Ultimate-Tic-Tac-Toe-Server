4dd2866ed365abace25061b9b5f8cae3db398d47
virnarula <virsvirus@gmail.com>
2020-06-20 16:34:16 -0500

    Implement server-side  request handling

--- index.js
+++ index.js
@@ -2,31 +2,66 @@
 const express = require("express");
 const WebSocket = require("ws");
 const SocketServer = require("ws").Server;
+const maingame = require("maingame");
+const requests = require("requests");
 
 console.log("Starting websockets server");
 const server = express().listen(3000);
 const wss = new SocketServer({ server });
 console.log("Server was opened on localhost:3000");
 var connections = 0;
+var player1;
+var player2;
+mainGame = new maingame();
 
 wss.on("connection", (ws) => {
-    if(connections === 2) {
-        ws.send("There are already 2 players on the server.");
-        ws.close();
+    switch(connections) {
+        case 0:
+            ws.send("You are player 1. You will be X. You will be able to start when players 2 joins.");
+            player1 = ws;
+            connections++;
+            console.log("[Server] a client was conneceted. Connections: " + connections);
+            break;
+        case 1:
+            ws.send("You are player X. You will be O.");
+            player1.send("Player 2 has joined. You can play your first move now");
+            player2 = ws;
+            connections++;
+            console.log("[Server] a client was conneceted. Connections: " + connections);
+            break;
+        default:
+            ws.send("There are already 2 players on the server.");
+            ws.close();
+            break;
+        
     }
-    connections++;
-    console.log("[Server] a client was conneceted. Connections: " + connections);
 
     ws.on("close", () => { 
         connections--;
         console.log("[Server] Client was disconnected. Connections: " + connections);
+        wss.clients.forEach(function each(client) {
+            client.send("A player has disconnected. The game is ending.");
+            client.close();
+        });
     });
 
     ws.on("message", (message) => { 
         console.log("[Server] received message %s", message);
+        var move = JSON.parse(message);
         wss.clients.forEach(function each(client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message);
+                console.log("Player " + move.getPlayer() +"played this move:");
+                console.log("board x:" + move.getBoardX());
+                console.log("board y:" + move.getBoardY());
+                console.log("rel x:" + move.getRelX());
+                console.log("rel y:" + move.getRelY());
+                if(maingame.playMove()) {
+                    console.log("Valid Move");
+                    client.send(true);
+                } else {
+                    console.log("Invalid Move");
+                    client.send(false);
+                }
             }
         });
     });
--- model/requests.js
+++ model/requests.js
@@ -1,8 +1,24 @@
 class move {
-    constructor(board_x, board_y, rel_x, rel_x, char) {
+    constructor(board_x, board_y, rel_x, rel_y, player) {
         this.board_x = board_x;
         this.board_y = board_y;
         this.rel_x = rel_x;
-        this.char = char;
+        this.rel_y = rel_y;
+        this.player = player;
+    }
+    getPlayer() {
+        return this.player;
+    }
+    getBoardX() {
+        return this.board_x;
+    }
+    getBoardY() {
+        return this.board_y;
+    }
+    getRelX() {
+        this.rel_x;
+    }
+    getRelY() {
+        this.rel_y;
     }
 }
